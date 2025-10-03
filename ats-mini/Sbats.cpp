51ab46b277b254d5dbd066e6242c9c5d1a66d0ac
PurpleCatGirl <165198062+PurpleCatGirl@users.noreply.github.com>
2025-10-03 07:27:32 -0400

    a bit of cleanup
    cleaning up comments etc in sbats.cpp

--- ats-mini/Sbats.cpp
+++ ats-mini/Sbats.cpp
@@ -7,8 +7,8 @@
 // newkirks recommended 200ms delay
 #define SBATS_POLL_TIME    200 // Tuning status polling interval (msecs)
 
-static uint32_t sbatsTime = millis();
-static uint32_t sbatsPress = 0;
+static uint32_t sbatsTime;
+static uint32_t sbatsPress;
 static uint16_t sbatsCurFreq;
 static uint16_t sbatsMinFreq;
 static uint16_t sbatsMaxFreq;
@@ -18,17 +18,17 @@ static bool     sbatsRev = false;
 static void sbatsInit(uint16_t freq) {
   const Band *band = getCurrentBand();
   const Step *step = getCurrentStep();
-  sbatsCurFreq      = freq;
-  sbatsMinFreq      = band->minimumFreq;
-  sbatsMaxFreq      = band->maximumFreq;
-  sbatsStep         = step->step;
-  sbatsTime         = millis();
-  sbatsPress        = 0;
+  sbatsCurFreq      = freq;               // start from wherever we are on the dial
+  sbatsMinFreq      = band->minimumFreq;  // current band lower edge
+  sbatsMaxFreq      = band->maximumFreq;  // current band upper edge
+  sbatsStep         = step->step;         // current step size
+  sbatsTime         = millis();           // reset the clock
+  sbatsPress        = 0;                  // clear any button-pressing
   if(band->bandName == "VHF") {           // override FM band limits for north america
-    sbatsMinFreq    = 8710;
-    sbatsMaxFreq    = 10790;
+    sbatsMinFreq    = 8710;               // no stations below 87.1 MHz
+    sbatsMaxFreq    = 10790;              // no stations above 107.9 MHz
   } else if(band->bandName == "MW1") {    // override AM band limits for north america
-    sbatsMinFreq    = 500;
+    sbatsMinFreq    = 500;                // no stations below 500 kHz
   }
   // clear parts of the screen  (screen size 320 x 170)
   spr.fillRect(90, 0, 240, 15, TH.bg);     // above band/mode, right of the sidebar
@@ -43,11 +43,9 @@ static bool sbatsMainLoop() {
   if(sbatsPress) {                                   // encoder previously pressed
     if(digitalRead(ENCODER_PUSH_BUTTON)==HIGH) {     // encoder released
       uint32_t delta = millis() - sbatsPress;
-      if(delta > 500) {                              // longish press
-        // cancel SB-ATS
+      if(delta > 375) {                              // longish press
         currentCmd = CMD_NONE;                       // cancel SBATS mode
       } else if(delta > 20) {                        // click
-        // change direction
         sbatsRev = !sbatsRev;                        // reverse direction
       }
       sbatsPress=0;                                  // clear press
@@ -79,7 +77,7 @@ static bool sbatsMainLoop() {
   }
   // set the tuner to the new / correct frequency
   rx.setFrequency(sbatsCurFreq);
-  // now update the display to show we've done something
+  // now update the display
   spr.fillRect(100, 35, 152, 52, TH.bg);
   spr.fillRect(0, 115, 320, 55, TH.bg);
   drawFrequency(sbatsCurFreq, FREQ_OFFSET_X, FREQ_OFFSET_Y, FUNIT_OFFSET_X, FUNIT_OFFSET_Y, 100);
@@ -94,7 +92,7 @@ void sbatsRun(uint16_t originalFreq) {
   // initialize SB-ATS
   sbatsInit(originalFreq);
   // run the main loop until it returns false
-  while(sbatsMainLoop());
+  while(sbatsMainLoop()) delay(5);
   // Restore current frequency
   rx.setFrequency(originalFreq);
   currentCmd=CMD_NONE;
